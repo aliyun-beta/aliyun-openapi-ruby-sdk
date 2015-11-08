a4498f77f17dd5c38d0236dc73dae061eaeddd5d
Gene Wu <genewoo@gmail.com>
2015-11-09 00:37:02 +0800

    Add region to configuration

--- lib/aliyun/openapi/core/configuration.rb
+++ lib/aliyun/openapi/core/configuration.rb
@@ -7,7 +7,7 @@ module Aliyun
     END_POINTS_FILE = File.expand_path('../../endpoints.yml', __FILE__)
 
     class Configuration #< OpenStruct
-      attr_accessor :server_address, :ssl_required, :access_key_id, :access_key_secret, :format
+      attr_accessor :server_address, :ssl_required, :access_key_id, :access_key_secret, :format, :region
       attr_reader :end_points
       def initialize
         @end_points = YAML.parse( File.read(END_POINTS_FILE) ).transform
@@ -15,8 +15,8 @@ module Aliyun
         @end_points.frozen? #make it read only
       end
 
-      def look_up_server(product, region = nil)
-        region ||= 'cn-hangzhou'
+      def look_up_server(product)
+        region = @region || 'cn-hangzhou'
         setting = @end_points.select {|k,v| v[:region_ids].include?(region)}
         setting.values[0][:products][product.to_s.gsub(/_/, '-').to_sym] unless setting.empty?
       end
